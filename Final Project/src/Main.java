6269eba7673b104b3d1f8ba6d88467b73a9941be
Nan Liu <nanliu@nans-mbp.lan>
2020-04-23 14:35:20 -0700

    add printout in Main

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -1,5 +1,4 @@
 
-import java.io.*;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
@@ -11,35 +10,11 @@ public class Main {
                 LocateRegistry.createRegistry(5010);
                 INameNode nameNode = new NameNode();
                 Naming.rebind("rmi://localhost:5010/namenode", nameNode);
+                System.out.println("DFS service is running.");
 
         } catch (Exception e) {
             System.out.println("Trouble: " + e);
         }
-//        String fileName = "Test";
-//        String content = "llsdsdasf";
-//        byte[] read_buffer = content.getBytes();
-//
-//        try {
-//            File newFile = new File(fileName);
-//            if (newFile.createNewFile()) {
-//                System.out.println("File created: " + newFile.getName());
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred.");
-//            e.printStackTrace();
-//        }
-//        try {
-//            BufferedWriter files_obj = new BufferedWriter(
-//                    new FileWriter(fileName, true));
-//            files_obj.write(content);
-//            files_obj.write("\n");
-//            files_obj.close();
-////            FileOutputStream data_file_obj = new FileOutputStream(fileName);
-////            data_file_obj.write(read_buffer);
-////            data_file_obj.close();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+
     }
 }
